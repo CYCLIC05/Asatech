6d91802f86f5fc2e23117986601ac210e81d4052
samuel <oyewales02.com>
2025-06-04 12:36:50 +0100

    install the tailwind dev dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import Navbar from './componenets/Navbar'
-import Hero from './componenets/Hero'
-import BusinessPartnersComponent from './componenets/BusinessPartnersComponent'
-import Monitoring from './componenets/Monitoring'
-import Services from './componenets/Services'
-import Newsletter from './componenets/Newsletter'
+import Navbar from './componenets/Navbar.jsx'
+import Hero from './componenets/Hero.jsx'
+import BusinessPartnersComponent from './componenets/BusinessPartnersComponent.jsx'
+import Monitoring from './componenets/Monitoring.jsx'
+import Services from './componenets/Services.jsx'
+import Newsletter from './componenets/Newsletter.jsx'
 const App = () => {
   return (
-    <main className=" overflow-x-hidden">
+    
+    <main className="overflow-x-hidden">
       <Navbar/>
       <Hero/>
       <BusinessPartnersComponent/>
